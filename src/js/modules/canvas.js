496a2b14f7ec55ddb594ed94e40928ab87b7b05c
Slave <>
2021-02-02 15:19:29 +0500

    add showing patterns

--- src/js/modules/canvas.js
+++ src/js/modules/canvas.js
@@ -3,24 +3,27 @@ import BasicNGon from './n-gon';
 import { RandNum } from './num';
 
 export default () => {
-    let canvass = document.querySelectorAll('canvas');
-    for (let canvas of canvass) {
-        let ctx = canvas.getContext('2d');
+    document.addEventListener('DOMContentLoaded', () => {
+        let canvass = document.querySelectorAll('canvas');
+        for (let canvas of canvass) {
+            console.log("Первый");
+            let ctx = canvas.getContext('2d');
 
-        // canvas.width = window.innerWidth;
-        // canvas.height = window.innerHeight;
-        
-        let render = new Render2D(canvas);
-        
-        function renderNGon(radius, n = 6) {
-            let NGon = new BasicNGon(n).scale(radius);
-            render.strokePath(NGon.corners);
+            // canvas.width = window.innerWidth;
+            // canvas.height = window.innerHeight;
+            
+            let render = new Render2D(canvas);
+            
+            function renderNGon(radius, n = 6) {
+                let NGon = new BasicNGon(n).scale(radius);
+                render.strokePath(NGon.corners);
+            }
+
+            for (let i = 0; i < 10; ++i)
+                renderNGon(i*10);
+
+            ctx.strokeStyle = 'white';
+            renderNGon(new RandNum(1, 10).multiply(10));
         }
-
-        for (let i = 0; i < 10; ++i)
-            renderNGon(i*10);
-
-        ctx.strokeStyle = 'white';
-        renderNGon(new RandNum(1, 10).multiply(10));
-    }
+    });
 }
--- src/js/modules/diagram-patterns-table.js
+++ src/js/modules/diagram-patterns-table.js
@@ -1,10 +1,50 @@
-import diagramPatterns from './diagram-patterns';
+import canvas from './canvas';
+import patterns from './diagram-patterns';
 
 export default () => {
-    let table = document.querySelector(".diag-homescreen__table-items");
-    document.querySelector(".diag-homescreen").onclick = () => {
-        diagramPatterns.add("Boruto", 10, ["strength", "intelligence", "agility"]);
-        table.insertAdjacentHTML("afterend", "<div style=\"background: red; width: 100px\"></div>");
-    };
+    
+    patterns.add("Naruto", 10, ["strength", "intelligence", "agility"]);
+    patterns.add("Boruto", 10, ["strength", "intelligence", "agility"]);
+    patterns.add("Soruto", 10, ["strength", "intelligence", "agility"]);
+    patterns.add("Seruto", 10, ["strength", "intelligence", "agility"]);
+    document.addEventListener('DOMContentLoaded', () => {
+        // e.preventDefault();
+        let table = document.querySelector(".diag-homescreen__table-content");
+        
+        let tableItems = document.createElement('a');
+        tableItems.classList.add('diag-homescreen__table-items');
+        tableItems.setAttribute('href', 'diagramm.html');
+
+        let tableDelete = document.createElement('div');
+        tableDelete.classList.add('diag-homescreen__table-delete');
+        tableItems.append(tableDelete);
+
+        let imgDelete = document.createElement('img');
+        imgDelete.setAttribute('src', 'img/delete.png');
+        imgDelete.setAttribute('alt', 'delete');
+        tableDelete.append(imgDelete);
+
+        let tableImg = document.createElement('div');
+        tableImg.classList.add('diag-homescreen__table-img');
+        tableItems.append(tableImg);
+
+        let imgCanvas = document.createElement('canvas');
+        imgCanvas.width = 160;
+        imgCanvas.height = 120;
+        tableImg.append(imgCanvas);
+
+        let tablTitle = document.createElement('div');
+        tablTitle.classList.add('diag-homescreen__table-title');
+        tableItems.append(tablTitle);
+
+        let tablTitleH2 = document.createElement('h2');
+        tablTitle.append(tablTitleH2);
+        
+        for (let pattern in patterns.patterns) {
+            let item = tableItems.cloneNode(true);
+            item.lastChild.firstChild.insertAdjacentText('beforeend', pattern);
+            table.append(item);
+        }
+    });
     //addPattern("cryptocurrency ", 5, ["profitability", "scalability", "notoriety", "reliability"]);
 }
