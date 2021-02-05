a94d6210533cbe2e6035d2f1ffb5877373c49ed0
Slave <>
2021-02-05 12:09:36 +0500

    add init environment

--- src/js/modules/init-local-storage.js
+++ src/js/modules/init-local-storage.js
@@ -1,6 +1,9 @@
 export default () => {
     // localStorage.clear();
-    if (localStorage.getItem("diagram patterns") == null) {
+    if (!localStorage.getItem("diagram patterns")) {
         localStorage.setItem("diagram patterns", JSON.stringify([]));
+    }
+    if (!localStorage.getItem("environment")) {
+        localStorage.setItem("environment", JSON.stringify({}));
     }
 }
