6119916b2ad29b82ee62544fa63be99ba45f2654
Slave <>
2021-02-05 11:52:45 +0500

    add gradient render and pattern data synchronization

--- src/js/modules/canvas.js
+++ src/js/modules/canvas.js
@@ -1,11 +1,11 @@
 import Render2D from './api-render';
 import BasicNGon from './n-gon';
 import { RandNum } from './num';
+import patterns from './diagram-patterns';
 
 export default () => {
     document.addEventListener('DOMContentLoaded', () => {
-        let canvass = document.querySelectorAll('canvas');
-        for (let canvas of canvass) {
+        for (let canvas of document.querySelectorAll('canvas')) {
             let ctx = canvas.getContext('2d');
 
             // canvas.width = window.innerWidth;
@@ -18,11 +18,24 @@ export default () => {
                 render.strokePath(NGon.corners);
             }
 
-            for (let i = 0; i < 10; ++i)
-                renderNGon(i*10);
+            let id;
+            let n = (id = canvas.dataset.patternId) != undefined ? patterns.get(id).size.axlesNumber : 6;
 
-            ctx.strokeStyle = 'white';
-            renderNGon(new RandNum(1, 10).multiply(10));
+            let grad = ctx.createLinearGradient(0, 0, 160, 120);
+            grad.addColorStop(0, 'red');
+            grad.addColorStop(0.5, 'cyan');
+            grad.addColorStop(1, 'blue');
+            ctx.strokeStyle = grad;
+            for (let i = 0; i < 10; ++i) {
+                renderNGon(i*10, n);
+            }
+
+            grad = ctx.createLinearGradient(0, 0, 160, 120);
+            grad.addColorStop(0, 'cyan');
+            grad.addColorStop(0.5, 'blue');
+            grad.addColorStop(1, 'red');
+            ctx.strokeStyle = grad;
+            renderNGon(new RandNum(1, 10).multiply(10), n);
         }
     });
 }
