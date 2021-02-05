38f93ea0c960eda0bbfadffc4b627e5833f78a34
Slave <>
2021-02-05 12:12:22 +0500

    add diagram maker

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 // Импорт js модулей
-import canvas from './modules/canvas';
 import initStorage from './modules/init-local-storage';
 import patternsTable from './modules/diagram-patterns-table';
 import patternsMaker from './modules/diagram-pattern-maker';
+import diagramMaker from './modules/diagram-maker';
+import canvas from './modules/canvas';
 
 initStorage();
 patternsTable();
 patternsMaker();
+diagramMaker();
 canvas();
