75548ca4549f145df1a71f0da140806a82f54bbd
Slave <>
2021-02-10 17:16:38 +0500

    add diagram; add display

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,1 @@
-// Импорт js модулей
-import initStorage from './modules/domain/init-local-storage';
-import patternsTable from './modules/diagram-patterns-table';
-import patternsMaker from './modules/diagram-pattern-maker';
-import diagramMaker from './modules/diagram-maker';
-import canvas from './modules/domain/canvas';
 
-initStorage();
-patternsTable();
-patternsMaker();
-diagramMaker();
-canvas();
